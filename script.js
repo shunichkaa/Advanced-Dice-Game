070f594f7f0c09791cb204fad9e35bc5c6c4182e
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-28 12:00:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,22 @@ const getHighestDuplicates = (arr) => {
   updateRadioOption(5, 0);
 };
 
+const detectFullHouse = (arr) => {
+  const counts = {};
 
+  for (const num of arr) {
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+
+  const hasThreeOfAKind = Object.values(counts).includes(3);
+  const hasPair = Object.values(counts).includes(2);
+
+  if (hasThreeOfAKind && hasPair) {
+    updateRadioOption(2, 25);
+  }
+
+  updateRadioOption(5, 0);
+};
 
 const resetRadioOptions = () => {
   scoreInputs.forEach((input) => {
@@ -115,6 +130,8 @@ const resetGame = () => {
   resetRadioOptions();
 };
 
+
+
 rollDiceBtn.addEventListener("click", () => {
   if (rolls === 3) {
     alert("You have made three rolls this round. Please select a score.");
@@ -124,7 +141,8 @@ rollDiceBtn.addEventListener("click", () => {
     rollDice();
     updateStats();
     getHighestDuplicates(diceValuesArr);
-  
+    detectFullHouse(diceValuesArr);
+
   }
 });
 
