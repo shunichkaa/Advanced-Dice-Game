974929aa8cf03a06ae18b7d56b71c64c1c5538ce
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-28 12:00:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ const updateScore = (selectedValue, achieved) => {
   scoreHistory.innerHTML += `<li>${achieved} : ${selectedValue}</li>`;
 };
 
+
 const getHighestDuplicates = (arr) => {
   const counts = {};
 
@@ -100,6 +101,25 @@ const detectFullHouse = (arr) => {
   updateRadioOption(5, 0);
 };
 
+const checkForStraights = (arr) => {
+  const sortedNumbersArr = arr.sort((a, b) => a - b);
+  const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
+  const uniqueNumbersStr = uniqueNumbersArr.join("");
+
+  const smallStraightsArr = ["1234", "2345", "3456"];
+  const largeStraightsArr = ["12345", "23456"];
+
+  if (smallStraightsArr.some(straight => uniqueNumbersStr.includes(straight))) {
+    updateRadioOption(3, 30);
+  }
+
+  if (largeStraightsArr.includes(uniqueNumbersStr)) {
+    updateRadioOption(4, 40);
+  }
+
+  updateRadioOption(5, 0);
+};
+
 const resetRadioOptions = () => {
   scoreInputs.forEach((input) => {
     input.disabled = true;
@@ -130,8 +150,6 @@ const resetGame = () => {
   resetRadioOptions();
 };
 
-
-
 rollDiceBtn.addEventListener("click", () => {
   if (rolls === 3) {
     alert("You have made three rolls this round. Please select a score.");
@@ -142,9 +160,12 @@ rollDiceBtn.addEventListener("click", () => {
     updateStats();
     getHighestDuplicates(diceValuesArr);
     detectFullHouse(diceValuesArr);
-
+    checkForStraights(diceValuesArr);
+    
   }
 });
+
+
 
 rulesBtn.addEventListener("click", () => {
   isModalShowing = !isModalShowing;
